ea2a4435d36e3d01188cc9c81054bb097ae4503c
papaizaodocleuse <tiagomendesneres232@gmail.com>
2025-02-11 23:45:43 +0000

    nivelavanci.c

--- .vscode/02/02.c
+++ .vscode/02/02.c
@@ -1,57 +1,92 @@
-#include <stdio.h>
-#include <string.h>
+#include <cstdio>
+#include <vector>
+#include <string>
 
-// Estrutura para representar uma cidade
+using namespace std;
+
 struct Cidade {
-    char codigo[4]; // Código da carta (ex: A01, B02)
-    int populacao;
-    float area;
-    float pib;
+    char codigo[4]; // Código da carta (ex: A01)
+    long long populacao;
+    double area;
+    long long pib;
     int pontos_turisticos;
+    double densidade_populacional; // Nova propriedade calculada
+    double pib_per_capita;        // Nova propriedade calculada
 };
 
+void calcularPropriedades(Cidade& cidade) {
+    // Calcula a densidade populacional (população / área)
+    if (cidade.area > 0) {
+        cidade.densidade_populacional = cidade.populacao / cidade.area;
+    } else {
+        cidade.densidade_populacional = 0; // Evita divisão por zero
+    }
+
+    // Calcula o PIB per capita (PIB / população)
+    if (cidade.populacao > 0) {
+        cidade.pib_per_capita = static_cast<double>(cidade.pib) / cidade.populacao;
+    } else {
+        cidade.pib_per_capita = 0; // Evita divisão por zero
+    }
+}
+
+void cadastrarCidade(vector<Cidade>& cartas) {
+    Cidade novaCidade;
+    printf("Digite o código da carta (ex: A01): ");
+    scanf("%s", novaCidade.codigo);
+    printf("Digite a população da cidade: ");
+    scanf("%lld", &novaCidade.populacao);
+    printf("Digite a área da cidade (em km²): ");
+    scanf("%lf", &novaCidade.area);
+    printf("Digite o PIB da cidade: ");
+    scanf("%lld", &novaCidade.pib);
+    printf("Digite o número de pontos turísticos: ");
+    scanf("%d", &novaCidade.pontos_turisticos);
+
+    // Calcula as novas propriedades
+    calcularPropriedades(novaCidade);
+
+    cartas.push_back(novaCidade);
+}
+
+void exibirCartas(const vector<Cidade>& cartas) {
+    for (const auto& cidade : cartas) {
+        printf("\nCódigo: %s\n", cidade.codigo);
+        printf("População: %lld\n", cidade.populacao);
+        printf("Área: %.2lf km²\n", cidade.area);
+        printf("PIB: %lld\n", cidade.pib);
+        printf("Pontos Turísticos: %d\n", cidade.pontos_turisticos);
+        printf("Densidade Populacional: %.2lf hab/km²\n", cidade.densidade_populacional);
+        printf("PIB per capita: %.2lf\n", cidade.pib_per_capita);
+    }
+}
+
 int main() {
-    const int NUM_ESTADOS = 8; // Estados de A a H
-    const int NUM_CIDADES_POR_ESTADO = 4; // 4 cidades por estado
-    struct Cidade cartas[NUM_ESTADOS][NUM_CIDADES_POR_ESTADO]; // Matriz para armazenar as cartas
+    vector<Cidade> cartas;
+    char opcao;
 
-    printf("Bem-vindo ao cadastro de cartas do Super Trunfo - Países!\n");
+    do {
+        printf("\nMenu:\n");
+        printf("1 - Cadastrar nova carta\n");
+        printf("2 - Exibir cartas cadastradas\n");
+        printf("3 - Sair\n");
+        printf("Escolha uma opção: ");
+        scanf(" %c", &opcao);
 
-    // Cadastro das cartas
-    for (int i = 0; i < NUM_ESTADOS; i++) {
-        char estado = 'A' + i; // Estado atual (A, B, C, ..., H)
-        for (int j = 0; j < NUM_CIDADES_POR_ESTADO; j++) {
-            printf("\nCadastrando cidade %c%d:\n", estado, j + 1);
-
-            // Preenche o código da carta
-            snprintf(cartas[i][j].codigo, 4, "%c%d", estado, j + 1);
-
-            // Solicita os dados da cidade
-            printf("População: ");
-            scanf("%d", &cartas[i][j].populacao);
-
-            printf("Área (em km²): ");
-            scanf("%f", &cartas[i][j].area);
-
-            printf("PIB (em bilhões de dólares): ");
-            scanf("%f", &cartas[i][j].pib);
-
-            printf("Número de pontos turísticos: ");
-            scanf("%d", &cartas[i][j].pontos_turisticos);
+        switch (opcao) {
+            case '1':
+                cadastrarCidade(cartas);
+                break;
+            case '2':
+                exibirCartas(cartas);
+                break;
+            case '3':
+                printf("Saindo...\n");
+                break;
+            default:
+                printf("Opção inválida! Tente novamente.\n");
         }
-    }
-
-    // Exibição dos dados das cartas
-    printf("\nDados das cidades cadastradas:\n");
-    for (int i = 0; i < NUM_ESTADOS; i++) {
-        for (int j = 0; j < NUM_CIDADES_POR_ESTADO; j++) {
-            printf("\nCidade %s:\n", cartas[i][j].codigo);
-            printf("  População: %d habitantes\n", cartas[i][j].populacao);
-            printf("  Área: %.2f km²\n", cartas[i][j].area);
-            printf("  PIB: %.2f bilhões de dólares\n", cartas[i][j].pib);
-            printf("  Pontos turísticos: %d\n", cartas[i][j].pontos_turisticos);
-        }
-    }
+    } while (opcao != '3');
 
     return 0;
 }
